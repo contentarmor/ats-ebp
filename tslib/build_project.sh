b62d33f458f1707285da8ab2efcd44da0dd2cdce
contentarmor <37691295+contentarmor@users.noreply.github.com>
2021-03-10 11:05:07 +0100

    Update ContentArmor's build script

--- tslib/build_project.sh
+++ tslib/build_project.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
 
+while [ "$1" != "" ]; do
+    PARAM=$1
+    case $PARAM in
+        -C)
+           make -f Makefile.ca clean
+           exit 0
+           ;;
+        -R)
+           make -f Makefile.ca clean
+           ;;
+        -X)
+           make -f Makefile.ca clean
+           ;;
+        *)
+           echo "usage: build_project.sh [-C | -R | -X]"
+           exit 1
+           ;;
+    esac
+    shift
+done
+
 make -f Makefile.ca
