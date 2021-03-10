86d264305f9df668ac6051e6e89dcd5c39b66d3b
contentarmor <37691295+contentarmor@users.noreply.github.com>
2021-03-10 11:11:44 +0100

    Add version

---
+++ tslib/tslib_version.c
@@ -0,0 +1,10 @@
+#include "tslib_version.h"
+
+#ifndef TSLIB_VERSION
+#define TSLIB_VERSION "1.0.1"
+#endif
+
+const char* get_tslib_version()
+{
+    return TSLIB_VERSION " (build: " __DATE__ " "__TIME__")";
+}
---
+++ tslib/tslib_version.h
@@ -0,0 +1,15 @@
+#ifndef _TSLIB_VERSION_H_
+#define _TSLIB_VERSION_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+const char* get_tslib_version();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
